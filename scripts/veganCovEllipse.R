ee74b3ba9735688b8839f3db6413340762e44d17
ednachiang <echiang3@wisc.edu>
2021-04-01 15:55:04 -0500

    Created UniFrac ordinations

--- scripts/veganCovEllipse.R
+++
@@ -1,5 +0,0 @@
-veganCovEllipse <- function(cov, center = c(0, 0), scale = 1, npoints = 100) {
-  theta <- (0:npoints) * 2 * pi/npoints
-  Circle <- cbind(cos(theta), sin(theta))
-  t(center + scale * t(Circle %*% chol(cov)))
-}
