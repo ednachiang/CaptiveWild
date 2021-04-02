a2be7ab061d6cbf53878765f76200dfdfc12b603
ednachiang <echiang3@wisc.edu>
2021-04-01 22:24:34 -0500

    Added phyla barplots

--- scripts/misc.R
+++ scripts/misc.R
@@ -9,3 +9,13 @@ g_legend <- function(a.gplot){
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   return(legend)}
+
+### Set equal number of breaks in faceted graph
+# https://stackoverflow.com/questions/28436855/change-the-number-of-breaks-using-facet-grid-in-ggplot2
+equal_breaks <- function(n = 3, s = 0.05, ...){
+  function(x){
+    # rescaling
+    d <- s * diff(range(x)) / (1+2*s)
+    round(seq(min(x)+d, max(x)-d, length=n), 0)
+  }
+}
